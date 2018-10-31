22dab1687fc330ed5a8f523e5452884bdf39389c
Robert Herley <rherley@stevens.edu>
2018-10-31 00:35:53 -0400

    started css for auth page

--- src/pages/auth/index.jsx
+++ src/pages/auth/index.jsx
@@ -1,5 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Auth = () => <div>Authentication Page</div>;
+import './styles.scss';
+import Tear from '../../components/common/Tear';
+
+class Auth extends Component {
+  state = {};
+
+  render() {
+    return (
+      <div className="auth-container">
+        <div className="auth-header">
+          <Tear className="icon" />
+          <span>Tyr</span>
+        </div>
+        <form className="auth-form">
+          <h1>Log In</h1>
+          <input type="text" name="name" />
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    );
+  }
+}
 
 export default Auth;
