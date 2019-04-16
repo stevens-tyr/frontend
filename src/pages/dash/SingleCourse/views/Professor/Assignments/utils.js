91af3066ac1a671fcab2e484bab72a602945b722
Robert Herley <rherley@stevens.edu>
2019-04-15 23:14:59 -0400

    remove redundant functions

--- src/pages/dash/SingleCourse/views/Professor/Assignments/utils.js
+++ src/pages/dash/SingleCourse/views/Professor/Assignments/utils.js
@@ -1,36 +1,11 @@
 /* eslint-disable import/prefer-default-export */
 import dayjs from 'dayjs';
 
-/**
- * If past == true, then this function returns all assignments whose dueDate is before today.
- * @param {List of Assignmets} assignments
- * @param {Boolean} past - a variable that determines if we want past assignments or upcoming assignments
- */
-const grabAssignments = (assignments, past = true) => {
-  if (assignments.length === 0) {
-    return null;
-  }
-  // choose whether we want past assignments or upcoming assignments
-  const filteredAssignments = assignments.filter(a => {
-    const due = dayjs(a.dueDate);
-    return past ? due.isBefore(dayjs()) : due.isAfter(dayjs());
-  });
-
-  // return assingments in desired format for antd table
-  return filteredAssignments.map(a => {
-    const { id, name, dueDate, language } = a;
-    return { key: id, name, dueDate, language };
-  });
-};
-
 const sortAssignments = (a, b, asc = true) => {
   const timeA = dayjs(a.dueDate);
   const timeB = dayjs(b.dueDate);
   if (timeA.isSame(timeB)) return 0;
-  if (asc) {
-    return timeA.isBefore(timeB) ? 1 : -1;
-  }
-  return timeA.isAfter(timeB) ? 1 : -1;
+  return timeA[asc ? 'isBefore' : 'isAfter'](timeB) ? 1 : -1;
 };
 
 const splitAssignments = assignments => {
@@ -38,20 +13,19 @@ const splitAssignments = assignments => {
     return null;
   }
 
-  // convert assignments in desired format for antd table
-  const past = [];
-  const future = [];
+  const pastAssignments = [];
+  const upcomingAssignments = [];
   assignments.sort((a, b) => sortAssignments(a, b, false));
   assignments.forEach(a => {
     const dueDate = dayjs(a.dueDate);
     // this assignment is a past assignment
     if (dueDate.isBefore(dayjs())) {
-      past.push(a);
+      pastAssignments.push(a);
     } else {
-      future.push(a);
+      upcomingAssignments.push(a);
     }
   });
-  return { upcomingAssignments: future, pastAssignments: past };
+  return { upcomingAssignments, pastAssignments };
 };
 
-export { grabAssignments, splitAssignments };
+export { splitAssignments };
