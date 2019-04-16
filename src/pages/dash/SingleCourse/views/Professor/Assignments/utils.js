1f127745a369b38c8c622b5846a3cbd168275c61
Taylor He <taylor.he7@gmail.com>
2019-04-15 22:57:10 -0400

    fixed bug where id assignment id wasn't being set in past assignments for professor view

--- src/pages/dash/SingleCourse/views/Professor/Assignments/utils.js
+++ src/pages/dash/SingleCourse/views/Professor/Assignments/utils.js
@@ -37,11 +37,13 @@ const splitAssignments = assignments => {
   if (assignments.length === 0) {
     return null;
   }
+  console.log('Splitting assignments:', assignments);
 
   // convert assignments in desired format for antd table
   const data = assignments.map(a => {
-    const { id, name, dueDate, language } = a;
-    return { key: id, name, dueDate, language };
+    const { _id, name, dueDate, language } = a;
+    console.log('id is', _id);
+    return { key: _id, name, dueDate, language };
   });
   const past = [];
   const future = [];
